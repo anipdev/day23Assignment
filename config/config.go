f09686d2f7f5f0e54c5757d0c95c54dd3af207f5
haniffaishalrafi <haniffaishalrafi@gmail.com>
2025-04-13 12:35:46 +0700

    initial commit

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	dbURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	DB, err = gorm.Open(mysql.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	log.Println("Database connection established successfully")
+}
---
+++ routes/router.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"day23Assignment/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/products", controllers.CreateProduct)
+	r.GET("/products", controllers.GetAllProducts)
+	r.GET("/products/:id", controllers.GetProductByID)
+	r.PUT("/products/:id", controllers.UpdateProduct)
+	r.DELETE("/products/:id", controllers.DeleteProduct)
+
+	r.GET("/inventory/:id", controllers.GetStockByID)
+	r.PUT("/inventory/:id", controllers.UpdateStock)
+
+	r.GET("/order/:id", controllers.GetOrderByID)
+	r.POST("/order/:id", controllers.CreateOrder)
+
+	return r
+}
